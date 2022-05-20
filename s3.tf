10893727086fab8cafa294e5d11e257c24c9698a
vakaniranganath <104778384+vakaniranganath@users.noreply.github.com>
2022-05-20 19:25:44 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,14 @@
-resource "aws_s3_bucket_object" "object" {
-for_each = fileset("myfiles/", "*")
-bucket = aws_s3_bucket.b.id
-key = each.value
-source = "myfiles/${each.value}"
-etag = filemd5("myfiles/${each.value}")
+resource "aws_s3_bucket" "b" {
+  bucket = "ranganath409"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+

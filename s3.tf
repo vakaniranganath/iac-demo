a63c3b49196efd5962dac9e5deb7b178c85d6782
vakaniranganath <104778384+vakaniranganath@users.noreply.github.com>
2022-05-20 19:56:59 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -9,3 +9,10 @@ resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.b.id
   acl    = "private"
 }
+resource "aws_s3_bucket_object" "object" {
+for_each = fileset("myfiles/", "*")
+bucket = aws_s3_bucket.b.id
+key = each.value
+source = "myfiles/${each.value}"
+etag = filemd5("myfiles/${each.value}")
+}

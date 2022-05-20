126d55363a60bc261b3af06bbcb88a2696a1fee6
vakaniranganath <104778384+vakaniranganath@users.noreply.github.com>
2022-05-20 19:28:40 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,14 +1,11 @@
 resource "aws_s3_bucket" "b" {
   bucket = "ranganath409"
-
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 }
-
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.b.id
   acl    = "private"
 }
-
